81b443e72537180b98a9c1677132dc0a64286b7e
Volyna <egeg.15@mail.ru>
2023-06-02 11:37:24 +0300

    Ошибки для русика

--- domstudent-miniapp/src/panels/Auth.js
+++ domstudent-miniapp/src/panels/Auth.js
@@ -11,44 +11,60 @@ const Auth = ({ id, go }) => {
 
 	const [password, setPassword] = useState('');
 
+
+
 	const Login = e => {
-		console.log({login, password});
+		console.log({ login, password });
+		fetchData();
+	}
+
+	const fetchData = async () => {
+		var formdata = new FormData();
+		formdata.append("login", login);
+		formdata.append("password", password);
+		formdata.append("vkid", "1411");
+
+		var requestOptions = {
+			method: 'POST',
+			body: formdata,
+			redirect: 'follow'
+		};
+
+		const response = await fetch("http://new-bokino.ru:49178/api/v1/user/auth", requestOptions)
+
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log('error', error));
 	}
 
 
-	//useEffect(() => 
-	//const ferchDate = async () => {
-	//		const respone = await fetch(' https://localhost:10888')
-	//		const data = await respone.json()
-	//		
-	//	}
-	//)
 
-	return(
-	<Panel id={id}>
-		<PanelHeader>Авторизация</PanelHeader>
+	return (
+		<Panel id={id}>
+			<PanelHeader>Авторизация</PanelHeader>
 
-		<Group header={<Header mode="primary">Вход в приложение</Header>}>
-			
-			<FormLayout>
-				<FormLayoutGroup mode="vertical">
-					<FormItem top="Логин">
-						<Input onChange={e => setLogin(e.target.value)}
-						value = {login}/>
-					</FormItem>
-					<FormItem top="Пароль">
-						<Input onChange={e => setPassword(e.target.value)}
-						value = {password}/>
-					</FormItem>
-				</FormLayoutGroup>
-			</FormLayout>
+			<Group header={<Header mode="primary">Вход в приложение</Header>}>
 
-			<CellButton onClick={e => Login(e)}>Войти</CellButton>
-			<CellButton onClick={BecomeAClient}>Стать клиентом</CellButton>
+				<FormLayout>
+					<FormLayoutGroup mode="vertical">
+						<FormItem top="Логин">
+							<Input onChange={e => setLogin(e.target.value)}
+								value={login} />
+
+						</FormItem>
+						<FormItem top="Пароль">
+							<Input onChange={e => setPassword(e.target.value)}
+								value={password} />
+						</FormItem>
+					</FormLayoutGroup>
+				</FormLayout>
+
+				<CellButton onClick={e => Login(e)}>Войти</CellButton>
+				<CellButton onClick={BecomeAClient}>Стать клиентом</CellButton>
 
 
-		</Group>
-	</Panel>)
+			</Group>
+		</Panel>)
 };
 
 
@@ -57,7 +73,7 @@ const BecomeAClient = () => {
 	bridge.send('VKWebAppOpenWallPost', {
 		owner_id: -220910322,
 		post_id: 4
-		})
+	})
 }
 
 Auth.propTypes = {
